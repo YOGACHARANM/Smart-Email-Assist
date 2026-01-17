2717e1d4b2b6a80ae009fb7e0e90d2ade4dbd90a
YOGACHARANM <yogacharan001@gmail.com>
2026-01-18 01:08:57 +0530

    Initial React + Vite project setup

---
+++ email-writer-react/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+    const [emailContent, setEmailContent] = useState('');
+const [tone, setTone] = useState('');
+const [generatedReply, setGeneratedReply] = useState('');
+const [loading, setLoading] = useState(false);
+const [error, setError] = useState('');
+  return (
+    <>
+   
+    </>
+  )
+}
+
+export default App
